8a7ab5ebcc915e266fc12a8c5dfaf44a4bd8f64a
Christian Fries <christian.fries@lstschenker.ch>
2016-12-08 18:39:50 +0100

    [TASK] Require backend_module extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,6 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-7.6.99',
+            'backend_module' => ''
         ],
         'conflicts' => [],
         'suggests' => [],

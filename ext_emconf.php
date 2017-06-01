eb69e3459760546650a4246302e5840fb24c24f9
Tobias Kummer <tobias.kummer@lstschenker.ch>
2017-06-01 10:07:02 +0200

    [RELEASE] Version 0.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'hallo@christian-fries.ch',
     'state' => 'alpha',
     'clearCacheOnLoad' => 1,
-    'version' => '0.1.0',
+    'version' => '0.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-8.7.99',
             'backend_module' => ''
         ],
         'conflicts' => [],

a7cdebfbf3ddee56f278c545e6635e8d89aeb33c
Christian Fries <hallo@christian-fries.ch>
2019-03-31 17:14:23 +0200

    [TASK] Require at least TYPO3 8 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'hallo@christian-fries.ch',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '1.0.1',
+    'version' => '2.0.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
-            'backend_module' => '0.7.1-0.99.99',
+            'typo3' => '8.7.0-9.5.99',
+            'backend_module' => '1.0.0-1.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
